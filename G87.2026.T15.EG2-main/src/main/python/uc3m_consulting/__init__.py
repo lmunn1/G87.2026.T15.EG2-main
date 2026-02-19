935fba670b5b013a9c433e65d32b9e1a2b4fde80
Linus Munn <lmunn@umass.edu>
2026-02-19 16:09:29 +0100

    Obliterated the directory ....

--- G87.2026.T15.EG2-main/src/main/python/uc3m_consulting/__init__.py
+++
@@ -1,6 +0,0 @@
-"""UC3M CONSULTING MODULE WITH ALL THE FEATURES REQUIRED FOR ACCESS CONTROL"""
-
-from .project_document import ProjectDocument
-from .enterprise_manager import EnterpriseManager
-from .enterprise_management_exception import EnterpriseManagementException
-from .enterprise_project import EnterpriseProject
