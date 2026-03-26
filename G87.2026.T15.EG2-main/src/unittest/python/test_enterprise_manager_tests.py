2c31c3a743d8993a1d46bac4790dc095acc77df0
Linus Munn <lmunn@umass.edu>
2026-03-26 20:35:02 +0100

    Removed print statements from tests

--- G87.2026.T15.EG2-main/src/unittest/python/test_enterprise_manager_tests.py
+++ G87.2026.T15.EG2-main/src/unittest/python/test_enterprise_manager_tests.py
@@ -25,7 +25,7 @@ class TestEnterpriseManager(unittest.TestCase):
             date='31/12/2027',
             budget=50000.00
         )
-        print(result)
+
         self.assertEqual(result, '5321c7d564405216c12f2e5b3b999718')
 
     def test_TC2(self):
@@ -39,7 +39,7 @@ class TestEnterpriseManager(unittest.TestCase):
         '21/05/2026',
         300000.00
         )
-        print(result)
+
         self.assertEqual(result, '4f8da926a15bd3730e4ac6b36a3021ff')
 
     def test_TC3(self):
@@ -54,7 +54,6 @@ class TestEnterpriseManager(unittest.TestCase):
         300000.00
         )
 
-        print(result)
         self.assertEqual(result, '9e740467be0d332761fcb2ca62e95d9b')
 
     def test_TC4(self):
@@ -68,7 +67,6 @@ class TestEnterpriseManager(unittest.TestCase):
         300000.00
         )
 
-        print(result)
         self.assertEqual(result, 'c40688a65c8da55fb336061ce5756e58')
 
     def test_TC5(self):
@@ -83,7 +81,6 @@ class TestEnterpriseManager(unittest.TestCase):
                 300000.00
             )
 
-            print(result)
             self.assertEqual(result, '75752c286fa78d286fe482d0a3e3e687')
 
     def test_TC6(self):
@@ -98,7 +95,6 @@ class TestEnterpriseManager(unittest.TestCase):
             300000.00
         )
 
-        print(result)
         self.assertEqual(result, 'ce8a4bf21407d6e589944a45c53decf3')
 
     def test_TC7(self):
@@ -113,7 +109,6 @@ class TestEnterpriseManager(unittest.TestCase):
             1000000.00
         )
 
-        print(result)
         self.assertEqual(result, 'd49b99e410bbf18b9454785b24066acf')
 
     def test_TC8(self):
